b9832781039960885dc521237e74f733f9747855
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-04-16 15:14:42 +0200

    Remove default version attribute

--- zenoh-c/all/conanfile.py
+++ zenoh-c/all/conanfile.py
@@ -8,7 +8,6 @@ required_conan_version = ">=1.53.0"
 
 class ZenohCPackageConan(ConanFile):
     name = "zenohc"
-    version = "0.10.1-rc"
     description = "C-API for Eclipse Zenoh: Zero Overhead Pub/sub, Store/Query and Compute protocol"
     tags = ["iot", "networking", "robotics", "messaging", "ros2", "edge-computing", "micro-controller"]
     license = "Apache License 2.0"
